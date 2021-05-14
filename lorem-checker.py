87470d30dec3e91c92d94f044a8a551761cdf7ce
Joel Majors <joelamajors@gmail.com>
2021-05-14 14:48:25 -0400

    adds more words to search

--- lorem-checker.py
+++ lorem-checker.py
@@ -3,6 +3,14 @@ import re
 import requests
 
 links = []
+pages = []
+
+lorem = set()
+
+lorem_string = "Lorem ipsum dolor amet consectetur adipiscing elit sed eiusmod tempor incididunt ut labore dolore magna aliqua Ut enim minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ea commodo consequat Duis aute irure dolor reprehenderit voluptate velit esse cillum dolore fugiat nulla pariatur Excepteur sint occaecat cupidatat proident sunt culpa qui officia deserunt mollit anim laborum".split()
+
+for w in lorem_string:
+    lorem.add(w)
 
 #opens urls
 f = open('urls.txt', 'r')
@@ -17,10 +25,16 @@ for line in f:
     soup = BeautifulSoup(req.content, 'html.parser')
     req.close()
 
-    # If text has has Lorem Ipsum, 
-    if soup.find(string=re.compile("lorem",re.IGNORECASE)):
-        links.append(line)
-    
+
+    # If text has has Lorem Ipsum,
+    for l in lorem:
+        if soup.find(string=re.compile(" " + l + " " ,re.IGNORECASE)):
+            pages.append(line)
+
+for p in pages:
+    if p not in links:
+        links.append(p)
+
 print("These links have Lorem Ipsum:")
 print('\n'.join(map(str,links)))
 
